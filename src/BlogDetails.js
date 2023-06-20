b8e63147814503021824630302cd7a51771c8233
karollZielinski <zielinskikarol022@gmail.com>
2023-06-20 12:16:25 +0200

    db_json

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -3,11 +3,11 @@ import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
-    const {data: blog, error, isPending} =useFetch('http://localhost:8000/blogs/' + id);
+    const {data: blog, error, isPending} =useFetch('https://karollzielinski.github.io/blooog/' + id);
     const history = useHistory();
 
     const handleClick= () => {
-        fetch('http://localhost:8000/blogs/' + blog.id, {
+        fetch('https://karollzielinski.github.io/blooog/' + blog.id, {
             method: 'DELETE'
         }).then(() => {
             history.push('/');
